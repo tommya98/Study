8cacbb7d8b5b63c5447c6afdee99be22beb8f41c
tommya98 <tommya98@naver.com>
2022-04-11 11:49:30 +0900

    2446 solved

---
+++ code/2446.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int a;
+
+  cin >> a;
+  for(int i = 0; i < a; i++) {
+    for(int j = 0; j < i; j++)
+      cout << " ";
+    for(int k = 0; k < 2 * (a - i) - 1; k++)
+      cout << "*";
+    cout << endl;
+  }
+  for(int i = 1; i < a; i++) {
+    for(int j = 0; j < a - i - 1; j++)
+      cout << " ";
+    for(int k = 0; k < 2 * i + 1; k++)
+      cout << "*";
+    cout << endl;
+  }
+    
+  return 0;
+}
