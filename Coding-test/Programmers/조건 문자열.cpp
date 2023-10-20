ec103ed196150d1babe4ec038d380f319c0d6c64
tommya98 <tommya98@naver.com>
2023-10-20 15:04:30 +0900

    프로그래머스 코딩 기초 트레이닝 Day 4 solved

---
+++ Coding-test/Programmers/조건 문자열.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+  if (ineq == ">" && eq == "=") {
+    return n >= m;
+  } else if (ineq == ">" && eq == "!") {
+    return n > m;
+  } else if (ineq == "<" && eq == "=") {
+    return n <= m;
+  } else {
+    return n < m;
+  }
+}
