13efb5bcdac36e349cd80f1b8f35bf9de74e96a1
tommya98 <tommya98@naver.com>
2023-11-10 19:33:23 +0900

    프로그래머스 코딩테스트 입문 Day 2 solved

---
+++ Coding-test/Programmers/두 수의 나눗셈.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+  int answer = 0;
+
+  answer = int(float(num1) / float(num2) * 1000);
+
+  return answer;
+}
---
+++ Coding-test/Programmers/분수의 덧셈.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int numer1, int denom1, int numer2, int denom2) {
+  vector<int> answer(2);
+
+  answer[0] = numer1 * denom2 + numer2 * denom1;
+  answer[1] = denom1 * denom2;
+
+  for (int i = 2; i <= answer[1]; i++) {
+    if (answer[0] % i == 0 && answer[1] % i == 0) {
+      answer[0] /= i;
+      answer[1] /= i;
+      i--;
+    }
+  }
+
+  return answer;
+}
