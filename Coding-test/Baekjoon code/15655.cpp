aa7b04f72380e7af772bef5a5555a992b202f0b9
tommya98 <tommya98@naver.com>
2023-12-01 12:30:38 +0900

    15655 solved

---
+++ Coding-test/Baekjoon code/15655.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int arr[8];
+int ans[8];
+
+void func(int n, int idx);
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N >> M;
+  for (int i = 0; i < N; i++) {
+    cin >> arr[i];
+  }
+  sort(arr, arr + N);
+  func(0, 0);
+
+  return 0;
+}
+
+void func(int n, int idx) {
+  if (n == M) {
+    for (int i = 0; i < M; i++) {
+      cout << ans[i] << " ";
+    }
+    cout << "\n";
+  }
+  for (int i = idx; i < N; i++) {
+    ans[n] = arr[i];
+    func(n + 1, i + 1);
+  }
+}
