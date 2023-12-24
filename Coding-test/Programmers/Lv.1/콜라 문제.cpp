7f3a96a4c09e10e08d2a04360991183fa970d51e
tommya98 <tommya98@naver.com>
2023-12-24 12:22:49 +0900

    프로그래머스 콜라 문제 solved

---
+++ Coding-test/Programmers/Lv.1/콜라 문제.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+  int answer = 0;
+
+  while (n >= a) {
+    n -= a;
+    answer += b;
+    n += b;
+  }
+
+  return answer;
+}
