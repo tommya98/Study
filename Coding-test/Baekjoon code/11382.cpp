b7d87fc7f8bfccf4ded1f807d9a4392cd8f8f9fc
tommya98 <tommya98@naver.com>
2024-01-02 14:41:18 +0900

    백준 11832 solved

---
+++ Coding-test/Baekjoon code/11382.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unsigned long long A, B, C;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> A >> B >> C;
+  cout << A + B + C;
+
+  return 0;
+}
