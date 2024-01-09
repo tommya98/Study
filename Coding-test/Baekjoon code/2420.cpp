c656bf298ed87c3ab9d275c8c742e8913be8d31e
tommya98 <tommya98@naver.com>
2024-01-10 00:13:58 +0900

    백준 2420 solved

---
+++ Coding-test/Baekjoon code/2420.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long N, M;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N >> M;
+  cout << abs(N - M);
+
+  return 0;
+}
