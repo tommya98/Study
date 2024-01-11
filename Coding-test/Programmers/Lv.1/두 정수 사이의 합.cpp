e3506c18929ed23ee275dd1d18779a37e30869cd
tommya98 <tommya98@naver.com>
2024-01-11 20:02:20 +0900

    프로그래머스 두 정수 사이의 합 solved

---
+++ Coding-test/Programmers/Lv.1/두 정수 사이의 합.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+  long long answer = 0;
+
+  if (a > b) {
+    int t = a;
+    a = b;
+    b = t;
+  }
+  for (int i = a; i <= b; i++) answer += i;
+
+  return answer;
+}
