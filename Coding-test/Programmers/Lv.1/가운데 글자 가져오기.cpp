48b83ea6b9258c23ed2d72a27b0e8015f0cca416
tommya98 <tommya98@naver.com>
2024-01-11 20:03:17 +0900

    프로그래머스 가운데 글자 가져오기 solved

---
+++ Coding-test/Programmers/Lv.1/가운데 글자 가져오기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+  string answer = "";
+
+  int len = s.size();
+  if (len % 2 == 0) {
+    answer += s[(len / 2) - 1];
+    answer += s[len / 2];
+  } else {
+    answer += s[len / 2];
+  }
+
+  return answer;
+}
