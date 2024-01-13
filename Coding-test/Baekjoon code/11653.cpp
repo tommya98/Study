2d3f99a9f05a551237e63d1c2a6ad7200f6da669
tommya98 <tommya98@naver.com>
2024-01-13 23:36:01 +0900

    백준 11653 solved

---
+++ Coding-test/Baekjoon code/11653.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N;
+  for (int i = 2; i <= N; i++) {
+    while (N % i == 0) {
+      cout << i << '\n';
+      N /= i;
+    }
+  }
+
+  return 0;
+}
