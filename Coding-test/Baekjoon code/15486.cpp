0b272960f0a7b0a7d44499910997024f1b329103
tommya98 <tommya98@naver.com>
2024-01-15 12:23:55 +0900

    백준 15486 solved

---
+++ Coding-test/Baekjoon code/15486.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+pair<int, int> arr[1500001];
+int dp[1500001];
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int N;
+  cin >> N;
+  for (int i = 1; i <= N; i++) cin >> arr[i].first >> arr[i].second;
+
+  for (int i = 1; i <= N + 1; i++) {
+    if (i + arr[i].first <= N + 1)
+      dp[i + arr[i].first] = max(dp[i + arr[i].first], dp[i] + arr[i].second);
+    dp[i + 1] = max(dp[i + 1], dp[i]);
+  }
+  cout << dp[N + 1];
+
+  return 0;
+}
