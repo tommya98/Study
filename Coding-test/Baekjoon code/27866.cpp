a4efd798fbd9d129b216703ff7c553f435edd532
tommya98 <tommya98@naver.com>
2024-02-19 09:02:06 +0900

    백준 27866 solved

---
+++ Coding-test/Baekjoon code/27866.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string S;
+int i;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> S;
+  cin >> i;
+  cout << S[i - 1];
+
+  return 0;
+}
