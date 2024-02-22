c12cb2337163d12485a6945166ffa3915d6114df
tommya98 <tommya98@naver.com>
2024-02-22 11:47:17 +0900

    백준 15964 solved

---
+++ Coding-test/Baekjoon code/15964.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long A, B;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> A >> B;
+  cout << (A + B) * (A - B);
+
+  return 0;
+}
