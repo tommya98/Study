07378bbb5ec222e08f8afcd45f56af4f679b1d4c
tommya98 <tommya98@naver.com>
2024-03-04 12:08:27 +0900

    백준 31472 solved

---
+++ Coding-test/Baekjoon code/31472.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int W;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> W;
+  cout << 4 * (int)sqrt(2 * W);
+
+  return 0;
+}
